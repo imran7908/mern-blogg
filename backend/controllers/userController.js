8a1e622b8a85ad8fe77a3153c98e193c85ff754e
Imran <imran1510043@gmail.com>
2022-08-16 08:36:44 +0600

    login functionality completed

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -49,4 +49,31 @@ const signup = async (req, res, next) => {
   return res.status(201).json({ user });
 };
 
-module.exports = { getAllUser, signup };
+const login = async (req, res, next) => {
+  const { email, password } = req.body;
+  let existingUser;
+
+  try {
+    existingUser = await User.findOne({ email });
+  } catch (err) {
+    return console.log(err);
+  }
+
+  if (!existingUser) {
+    return res.status(404).json({
+      message: "Couldn't find any user with this email!",
+    });
+  }
+
+  const isPasswordCorrect = bcrypt.compareSync(password, existingUser.password);
+  if (!isPasswordCorrect) {
+    return res.status(400).json({
+      message: "Incorrect password!",
+    });
+  }
+  return res.status(200).json({
+    message: "Login successful!",
+  });
+};
+
+module.exports = { getAllUser, signup, login };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,11 +2,12 @@
 const express = require("express");
 
 // internal imports
-const { getAllUser, signup } = require("../controllers/userController");
+const { getAllUser, signup, login } = require("../controllers/userController");
 
 const router = express.Router();
 
 router.get("/", getAllUser);
-router.post("/signup", signup)
+router.post("/signup", signup);
+router.post("/login", login);
 
 module.exports = router;
