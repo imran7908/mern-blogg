4df3246a7b80aba20b14ae6e1a06257428751b50
Imran <imran1510043@gmail.com>
2022-08-17 12:02:40 +0600

    header done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,29 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Auth from "./components/Auth";
+import Header from "./components/Header";
+import Blogs from "./components/Blogs";
+import UserBlogs from "./components/UserBlogs";
+import BlogDetail from "./components/BlogDetail";
+import AddBlog from "./components/AddBlog";
+
 const App = () => {
   return (
-    <div>Hello World</div>
+    <React.Fragment>
+      <header>
+        <Header />
+      </header>
+      <main>
+        <Routes>
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/myBlogs" element={<UserBlogs />} />
+          <Route path="/myBlogs/:id" element={<BlogDetail />} />
+          <Route path="/blogs/add" element={<AddBlog />} />
+        </Routes>
+      </main>
+    </React.Fragment>
   );
-}
+};
 
 export default App;
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,64 @@
-import React from 'react'
+import React, { useState } from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Box,
+  Button,
+  Tabs,
+  Tab,
+} from "@mui/material";
+import { Link } from "react-router-dom";
 
 const Header = () => {
+  const [value, setValue] = useState();
   return (
-    <div>Header</div>
-  )
-}
+    <AppBar position="sticky" sx={{ background: "green" }}>
+      <Toolbar>
+        <Typography variant="h4">BLOGG</Typography>
+        <Box display="flex" marginLeft="auto" marginRight="auto">
+          <Tabs
+            textColor="inherit"
+            value={value}
+            onChange={(e, val) => setValue(val)}
+          >
+            <Tab LinkComponent={Link} to="/blogs" label="All Blogs" />
+            <Tab LinkComponent={Link} to="/myBlogs" label="My Blogs" />
+          </Tabs>
+        </Box>
 
-export default Header
+        <Box display="flex" marginLeft="auto">
+          <Button
+            LinkComponent={Link}
+            to="/auth"
+            variant="contained"
+            sx={{ margin: 1, borderRadius: 10 }}
+            color="warning"
+          >
+            Login
+          </Button>
+          <Button
+            LinkComponent={Link}
+            to="/auth"
+            variant="contained"
+            sx={{ margin: 1, borderRadius: 10 }}
+            color="warning"
+          >
+            Signup
+          </Button>
+          <Button
+            LinkComponent={Link}
+            to="/auth"
+            variant="contained"
+            sx={{ margin: 1, borderRadius: 10 }}
+            color="warning"
+          >
+            Logout
+          </Button>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
