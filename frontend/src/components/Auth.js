f13dfb3eaab44600c371ce19e410f07534ec777c
Imran <imran1510043@gmail.com>
2022-08-18 19:50:09 +0600

    my blogs functionality completed

--- frontend/src/components/Auth.js
+++ frontend/src/components/Auth.js
@@ -41,11 +41,13 @@ const Auth = () => {
     console.log(inputs);
     if (isSignup) {
       sendRequest("signup")
+        .then((data) => localStorage.setItem("userId", data.user._id))
         .then(() => dispath(authActions.login()))
         .then(() => navigate("/blogs"))
         .then((data) => console.log(data));
     } else {
       sendRequest()
+        .then((data) => localStorage.setItem("userId", data.user._id))
         .then(() => dispath(authActions.login()))
         .then(() => navigate("/blogs"))
         .then((data) => console.log(data));
--- frontend/src/components/UserBlogs.js
+++ frontend/src/components/UserBlogs.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Blog from "./Blog";
 
 const UserBlogs = () => {
-  return (
-    <div>UserBlogs</div>
-  )
-}
+  const [blogs, setBlogs] = useState();
+  const id = localStorage.getItem("userId");
+  const sendRequest = async () => {
+    const res = await axios
+      .get(`http://127.0.0.1:5000/blogs/user/${id}`)
+      .catch((err) => console.log(err));
+    const data = await res.data;
+    return data;
+  };
 
-export default UserBlogs
+  useEffect(() => {
+    sendRequest().then((data) => setBlogs(data.blogs.blogs));
+  }, []);
+  console.log(blogs);
+
+  return (
+    <div>
+      {blogs &&
+        blogs.map((blog, index) => (
+          <Blog
+            title={blog.title}
+            description={blog.description}
+            imageURL={blog.image}
+            userName={blog.user.name}
+          />
+        ))}
+    </div>
+  );
+};
+
+export default UserBlogs;
